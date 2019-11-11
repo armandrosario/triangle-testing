34fd4cf262982c4f20fcefa19015d69be33efd4b
Armand Rosario <31975821+armandrosario@users.noreply.github.com>
2019-11-10 21:42:16 -0600

    Create Point.java

---
+++ Point.java
@@ -0,0 +1,18 @@
+public class Point {
+	private double x,y;
+	
+	public Point() {
+		x=0;
+		y=0;
+	}
+	public Point(double a,double b) {
+		x=a;
+		y=b;
+	}
+	public double getX() {
+		return x;
+	}
+	public double getY() {
+		return y;
+	}
+}
